42b56aaa301b5417e27e9240724b270c5efd5d9f
kateBy <nonameisiam@yandex.ru>
2016-06-06 20:26:49 +0300

    Добавлен перевод числительных, подрядковых числительных

--- basics.cpp
+++ basics.cpp
@@ -567,30 +567,30 @@ void get_number(int32_t number,string &str)
 	if(number<0)
 		{
 		number*=-1;
-		str="negative ";
+		str=string(negative);
 		}
 	switch(number)
 		{
-		case 0:str="zero";break;
-		case 1:str="one";break;
-		case 2:str="two";break;
-		case 3:str="three";break;
-		case 4:str="four";break;
-		case 5:str="five";break;
-		case 6:str="six";break;
-		case 7:str="seven";break;
-		case 8:str="eight";break;
-		case 9:str="nine";break;
-		case 10:str="ten";break;
-		case 11:str="eleven";break;
-		case 12:str="twelve";break;
-		case 13:str="thirteen";break;
-		case 14:str="fourteen";break;
-		case 15:str="fifteen";break;
-		case 16:str="sixteen";break;
-		case 17:str="seventeen";break;
-		case 18:str="eighteen";break;
-		case 19:str="nineteen";break;
+		case 0:str=string(zero);break;
+		case 1:str=string(one);break;
+		case 2:str=string(two);break;
+		case 3:str=string(three);break;
+		case 4:str=string(four);break;
+		case 5:str=string(five);break;
+		case 6:str=string(six);break;
+		case 7:str=string(seven);break;
+		case 8:str=string(eight);break;
+		case 9:str=string(nine);break;
+		case 10:str=string(ten);break;
+		case 11:str=string(eleven);break;
+		case 12:str=string(twelve);break;
+		case 13:str=string(thirteen);break;
+		case 14:str=string(fourteen);break;
+		case 15:str=string(fifteen);break;
+		case 16:str=string(sixteen);break;
+		case 17:str=string(seventeen);break;
+		case 18:str=string(eighteen);break;
+		case 19:str=string(nineteen);break;
 		default:
 			{
 			if(number>=1000000000)
@@ -598,7 +598,7 @@ void get_number(int32_t number,string &str)
 				string nm;
 				get_number(number/1000000000,nm);
 				str+=nm;
-				str+=" billion";
+				str+=string(billion);
 				if(number%1000000000!=0)
 					{
 					str+=" ";
@@ -612,7 +612,7 @@ void get_number(int32_t number,string &str)
 				string nm;
 				get_number(number/1000000,nm);
 				str+=nm;
-				str+=" million";
+				str+=string(million);
 				if(number%1000000!=0)
 					{
 					str+=" ";
@@ -626,7 +626,7 @@ void get_number(int32_t number,string &str)
 				string nm;
 				get_number(number/1000,nm);
 				str+=nm;
-				str+=" thousand";
+				str+=string(thousand);
 				if(number%1000!=0)
 					{
 					str+=" ";
@@ -640,7 +640,7 @@ void get_number(int32_t number,string &str)
 				string nm;
 				get_number(number/100,nm);
 				str+=nm;
-				str+=" hundred";
+				str+=string(hundred);
 				if(number%100!=0)
 					{
 					str+=" ";
@@ -653,14 +653,14 @@ void get_number(int32_t number,string &str)
 				{
 				switch(number/10)
 					{
-					case 2:str="twenty";break;
-					case 3:str="thirty";break;
-					case 4:str="forty";break;
-					case 5:str="fifty";break;
-					case 6:str="sixty";break;
-					case 7:str="seventy";break;
-					case 8:str="eighty";break;
-					case 9:str="ninety";break;
+					case 2:str=string(twenty);break;
+					case 3:str=string(thirty);break;
+					case 4:str=string(forty);break;
+					case 5:str=string(fifty);break;
+					case 6:str=string(sixty);break;
+					case 7:str=string(seventy);break;
+					case 8:str=string(eighty);break;
+					case 9:str=string(ninety);break;
 					}
 				if(number%10!=0)
 					{
@@ -689,24 +689,8 @@ void get_ordinal(int32_t number,string &str,bool shorten)
 			str="-";
 			}
 		add_long_to_string(number,str);
-		switch(number%10)
-			{
-			case 1:
-				if(number%100==11)str+="th";
-				else str+="st";
-				break;
-			case 2:
-				if(number%100==12)str+="th";
-				else str+="nd";
-				break;
-			case 3:
-				if(number%100==13)str+="th";
-				else str+="rd";
-				break;
-			default:
-				str+="th";
-				break;
-			}
+		str+=string(ord_th);
+		
 		return;
 		}
 
@@ -714,50 +698,33 @@ void get_ordinal(int32_t number,string &str,bool shorten)
 	if(number<0)
 		{
 		number*=-1;
-		str="Negative ";
+		str=string(Negative);
 		}
 	switch(number)
 		{
-		case 0:str="Zeroth";break;
-		case 1:str="First";break;
-		case 2:str="Second";break;
-		case 3:str="Third";break;
-		case 4:str="Fourth";break;
-		case 5:str="Fifth";break;
-		case 6:str="Sixth";break;
-		case 7:str="Seventh";break;
-		case 8:str="Eighth";break;
-		case 9:str="Ninth";break;
-		case 10:str="Tenth";break;
-		case 11:str="Eleventh";break;
-		case 12:str="Twelfth";break;
-		case 13:str="Thirteenth";break;
-		case 14:str="Fourteenth";break;
-		case 15:str="Fifteenth";break;
-		case 16:str="Sixteenth";break;
-		case 17:str="Seventeenth";break;
-		case 18:str="Eighteenth";break;
-		case 19:str="Nineteenth";break;
+		case 0:str=string(Zeroth);break;
+		case 1:str=string(First);break;
+		case 2:str=string(Second);break;
+		case 3:str=string(Third);break;
+		case 4:str=string(Fourth);break;
+		case 5:str=string(Fifth);break;
+		case 6:str=string(Sixth);break;
+		case 7:str=string(Seventh);break;
+		case 8:str=string(Eighth);break;
+		case 9:str=string(Ninth);break;
+		case 10:str=string(Tenth);break;
+		case 11:str=string(Eleventh);break;
+		case 12:str=string(Twelfth);break;
+		case 13:str=string(Thirteenth);break;
+		case 14:str=string(Fourteenth);break;
+		case 15:str=string(Fifteenth);break;
+		case 16:str=string(Sixteenth);break;
+		case 17:str=string(Seventeenth);break;
+		case 18:str=string(Eighteenth);break;
+		case 19:str=string(Nineteenth);break;
 		default:
 			add_long_to_string(number,str);
-			switch(number%10)
-				{
-				case 1:
-					if(number%100==11)str+="th";
-					else str+="st";
-					break;
-				case 2:
-					if(number%100==12)str+="th";
-					else str+="nd";
-					break;
-				case 3:
-					if(number%100==13)str+="th";
-					else str+="rd";
-					break;
-				default:
-					str+="th";
-					break;
-				}
+                        str+=string(ord_th);
 			break;
 		}
 }
